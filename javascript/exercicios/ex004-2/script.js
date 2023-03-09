99909534133327bc181a5edb80db02eb8b999eb2
José Miquéias <jmiqueiasns1302@gmail.com>
2023-03-09 11:35:36 -0300

    exercício 04 de JS Guanabara

---
+++ javascript/exercicios/ex004-2/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    
+    if (num.value.length != 0){
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${c*n}`
+            item.setAttribute('value', `v${c}`)
+            tab.appendChild(item)
+
+            c++
+        }
+    } else{
+        window.alert('Por favor digite um número!')
+    }
+}
